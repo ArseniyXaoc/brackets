34f2f73d2c4ad105ef54689b954aa50f9a1dc397
ArseniyXaoc <xaoc190789@gmail.com>
2020-10-14 21:16:14 +0300

    complite

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
+
+//const bracketsConfig = [['(', ')'], ['[', ']'], ['{', '}']];;
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brCfg = str.split('');
+  let ouput;
+  function del(str) {    
+    ouput = str.slice();
+    bracketsConfig.forEach(item => {
+      for (let i = 0; i < ouput.length; i++) {
+        if (ouput[i] == item[0] && ouput[i + 1] == item[1]) ouput.splice(i, 2);
+      }
+    })
+    if(ouput.length == 0) return true;
+    if(ouput.toString() == str.toString()) return false;
+    return del(ouput);
+  }
+let result = del(brCfg);
+return result;  
 }
+
+// your solution
+
+
+//console.log(check('([{}])', bracketsConfig));
